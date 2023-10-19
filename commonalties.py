fcc0aaf2db67a4ee9f5979c53c6632c8ce4b2361
Salah eddine berret <146879159+SBerret3301@users.noreply.github.com>
2023-10-19 02:22:44 +0100

    Update commonalties.py

--- commonalties.py
+++ commonalties.py
@@ -3,5 +3,8 @@ y = 0
 for i in range(1,x+1) :
     if x % i == 0 :
         y += 1
+print("the number of divisors is ", y ,"and they are : ")
+for i in range(1,x+1) :
+    if x % i == 0 :
+        y += 1
         print (i)
-print("the number of divisors is : ",y)
